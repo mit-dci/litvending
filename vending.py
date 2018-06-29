617d9a5bf53c39139d4638fd7f9a303656a7d05a
Trey Del Bonis <j.delbonis.3@gmail.com>
2018-06-29 16:00:51 -0400

    Made it actually send the quarter deposit signal multiple times if there's multiple coins to send.

--- vending.py
+++ vending.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 
-import os
-import os.path as paths
 import sys
-
-import json
 
 gpio = None
 try:
@@ -13,44 +9,8 @@ try:
 except:
 	print('RPi library not found.  We\'re probably on a dev machine.  Moving on...')
 
+import lvconfig
 import litrpc
-
-default_config = {
-	'pin_num': -1,
-	'pin_high_time': 1,
-	'unit_cost_sat': 1000,
-	'coin_type_id': 1, # test btc
-	'lit_ip': '127.0.0.1',
-	'lit_port': '8001', # FIXME I don't think this is right.
-	'pool_rate': 5
-}
-
-def get_cfg_path():
-	if os.getenv('LITVENDING_CONFIG') is not None:
-		return paths.expanduser(os.getenv('LITVENDING_CONFIG'))
-	else:
-		return paths.join(os.getenv('HOME'), '.litvending', 'config.json')
-
-def load_config():
-
-	# Figure out what the path is.
-	cfgpath = get_cfg_path()
-
-	# Here is where we decide if we need to create a new config.
-	if not paths.exists(cfgpath):
-		print('Config not found, creating at', cfgpath)
-		dir = paths.dirname(cfgpath)
-		if dir != '':
-			os.makedirs(dir, exist_ok=True)
-		with open(cfgpath, 'w') as f:
-			f.write(json.dumps(default_config, indent=4))
-		return default_config
-	else:
-		# This is easy, just load it.
-		with open(cfgpath, 'r') as f:
-			return json.loads(f.read())
-
-conn = None
 
 def check_deposit(cointype):
 	bals = conn.balance()
@@ -92,10 +52,12 @@ def main(cfg):
 			print('Balance is now', bal, 'got a spend of', diff, 'worth', units_to_insert, 'with an extra', extra, 'left over')
 			if gpio != None:
 
-				# Just turn it on, wait a bit, and turn it off.
-				gpio.output(cfg['pin_num'], gpio.HIGH)
-				time.sleep(cfg['pin_high_time'])
-				gpio.output(cfg['pin_num'], gpio.LOW)
+				for i in range(units_to_insert):
+					# Just turn it on, wait a bit, and turn it off.
+					gpio.output(cfg['pin_num'], gpio.HIGH)
+					time.sleep(cfg['pin_high_time'])
+					gpio.output(cfg['pin_num'], gpio.LOW)
+					time.sleep(cfg['deposit_delay_time'])
 
 			else:
 				print('Not running on RPi, doing nothing!')
