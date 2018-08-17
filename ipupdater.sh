d23b53f12f5b19ea1fd239f24b20587b800699cd
Trey Del Bonis <j.delbonis.3@gmail.com>
2018-08-17 15:52:40 -0400

    Let you pick which iface to report the IP address of.

--- ipupdater.sh
+++ ipupdater.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-sleeptime=$1
-hostaddr=$2
-destfile=$3
+iface=$1
+sleeptime=$2
+hostaddr=$3
+destfile=$4
 
 while true; do
-	ip=$(ip a show dev wlp1s0 | grep 'inet ' | sed 's/\// /g' | awk '{ print $2 }')
+	ip=$(ip a show dev $iface | grep 'inet ' | sed 's/\// /g' | awk '{ print $2 }')
 	echo $ip | ssh $hostaddr bash -c "cat - > $destfile"
 	sleep $sleeptime
 done
